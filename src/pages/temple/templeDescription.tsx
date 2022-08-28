778e08a27313a6f32496272cdffe04c680c8f43a
Rob Kiers <rob.kiers@getthere.nl>
2022-08-28 22:18:13 +0200

    upd

--- src/pages/temple/templeDescription.tsx
+++ src/pages/temple/templeDescription.tsx
@@ -3,29 +3,41 @@ function TempleDescription() {
     <div>
       <h1>The Temple</h1>
       <p>
-
+        The temple of the Moonrise Hermitage sits close to the top of the same hill that the commons is constructed
+        against. It is a smaller temple near a number of waterfalls, flanked by two smaller towers and one larger one
+        that reaches up to the cliff side besides it.
       </p>
       <p>
-        A temple sits near a number of waterfalls. It is a modest temple sat at
-        an idyllic part of the valley near the waterfalls and rivers. Walkways
-        span over the streams and a tower rises up near the cliff side. This
-        tower holds a library with texts on Elune and their prayers. Recently a
-        bridge has been grown up to the upper ledge nearby. In the back there is
-        a small entrance into the cliffs, that through a number of caverns and
-        tunnels leads to the commons below.
+        The usual approach from the commons leads you to a fork in the road. One path leads to the Moonspeaker's mansion,
+        a little further up and the other leads to a set of stairs. Following the stairs up you come to the circular
+        dome that is the temple proper. Inside a small stream flows through it that leads to one of the aforementioned
+        waterfalls. An elevated platform gives space for the Sisterhood to perform their sermons.
       </p>
       <p>
-        A larger house nearby is the house of the Moonspeaker. It holds the
-        personal rooms of the leading priestess of the Moonrise Hermitage as
-        well as space for a number of novices and servants dedicated to her. It
-        currently sits empty.
+        Three doors lead to the adjacent building that the Sisterhood use in their nightly business. It is half open to
+        the elements outside with a secondary platform that holds a few smaller rooms. This building is set up between
+        the walkways towards the two smaller towers. The northern tower is the inner sanctum of the Sisterhood, where
+        they perform their sacred rituals to Elune that only the Priestesses are privy to. The other tower stands on a
+        lone outjutting of rock, reachable by a bridge that spans the small crevice with the path leading to the
+        Moonspeaker's mansion. This tower is generally off-limit as well, to those whom are not part of the Moonrise
+        Hermitage.
       </p>
       <p>
-        An observatory stands on the cliffs above the temple, its glass roof
-        allowing direct vision of the stars. Patterns engraved in the glass help
-        discern what is and what will be. Along the ground floor there are star
-        maps and tomes of knowledge on the constellations and the lore of the
-        zodiacs.
+        From the circular temple, one can also go through an open door towards the large tower. The different floors are
+        also used by the Sisterhood, some as residence and others as library. A statue of Haidene adorns it side, her
+        gaze set towards the skies. A tree grows around this tower to form a newly made bridge that goes towards the
+        cliff side and towards a path further up the mountain to an observatory.
+      </p>
+      <p>
+        An observatory stands on the cliffs above the temple, its glass roof allowing direct vision of the stars.
+        Patterns engraved in the glass help discern what is and what will be. Along the ground floor there are star maps
+        and tomes of knowledge on the constellations and the lore of the zodiacs.
+      </p>
+      <p>
+        Finally there is a small area behind the temple, where a path leads to the tunnels that connect to the commons
+        further down below. A different path leads to the large house of the Moonspeaker. It holds the personal rooms of
+        the leading priestess of the Moonrise Hermitage as well as space for a number of novices and servants dedicated
+        to her. It currently sits empty.
       </p>
     </div>
   );
